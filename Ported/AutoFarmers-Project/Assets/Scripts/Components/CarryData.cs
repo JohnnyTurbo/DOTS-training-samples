c8421d1ae26b921ca4303e1acdddbd83e8dbd721
Harris McKay <harris.mckay@unity3d.com>
2022-03-31 13:37:45 -0700

    Removed unused field

--- Ported/AutoFarmers-Project/Assets/Scripts/Components/CarryData.cs
+++ Ported/AutoFarmers-Project/Assets/Scripts/Components/CarryData.cs
@@ -1,11 +1,9 @@
 ﻿using Unity.Entities;
-using Unity.Transforms;
 
 namespace AutoFarmers
 {
     public struct CarryData : IComponentData
     {
         public Entity carriedEntity;
-        public Translation carriedTranslation; 
     }
 }
